e56563d8a0d05e372389877d99b816be2757f5c6
sharafuddin3526-dotcom <sharafuddin3526@gmail.com>
2026-05-03 01:37:02 +0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+export const BOT_TOKEN = "8529411138:AAHoxy_H8vWMS7A4uT188-6jehySe2uj_mI";
+
+export const ADMIN_ID = 8136997138;
+
+// Must join channel
+export const METHOD_CHANNEL = "@Global_Method_Channel";
+
+// Optional UI link
+export const MAIN_CHANNEL = "https://t.me/+75BQ2Qw9UZI4OTM1M";
